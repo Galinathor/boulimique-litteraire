c23b70f74f0b89258a52975bc3bd2b8a21fd7e76
Alexandre <bakalexandre5@gmail.com>
2015-10-19 16:44:56 +0200

    Add comments on recipe
    Signed-off-by: Alexandre <bakalexandre5@gmail.com>

--- src/main/webapp/scripts/controllers/recipeCtrl.js
+++ src/main/webapp/scripts/controllers/recipeCtrl.js
@@ -1,14 +1,32 @@
 (function () {
     angular.module('boulimiqueLitteraire').
-        controller('RecipeCtrl', ['$scope', 'RecipeFactory', '$routeParams',
-        function ($scope, RecipeFactory, $routeParams) {
+        controller('RecipeCtrl', ['$scope', 'RecipeFactory', 'RecipeCommentFactory', '$routeParams',
+        function ($scope, RecipeFactory, RecipeCommentFactory, $routeParams) {
 
         $scope.recipe = {};
+        $scope.comment = {};
+        $scope.comments = [];
+
+        $scope.sendComment = function(comment){
+            RecipeCommentFactory.save(comment).
+            then(function(data){
+                $scope.comments.push(data);
+                $scope.comment = {};
+            }, function(error){
+                console.log(error.data.message);
+            })
+        }
 
         function init(){
             RecipeFactory.getById($routeParams.id).
             then(function(data){
                 $scope.recipe = data;
+            }, function(error){
+                console.log(error.data.message);
+            })
+            RecipeCommentFactory.getAll().
+            then(function(data){
+                $scope.comments = data;
             }, function(error){
                 console.log(error.data.message);
             })
